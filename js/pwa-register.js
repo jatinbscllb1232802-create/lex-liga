901c9fc21b245f2ce431c742711bb6814110165a
Jatin Narula <jatin.bscllb1232802@nfsu.ac.in>
2026-09-16 18:51:34 +0530

    Register service worker for PWA

---
+++ js/pwa-register.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./sw.js').catch(function (e) {
+      console.warn('SW register failed', e);
+    });
+  });
+}
